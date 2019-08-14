b4fff357c6a1dc80ebf4d34a78aeaa8f726e7222
Alumno <Alumno@PC04>
2019-08-14 12:25:25 -0500

    v2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,12 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1, num2,resultado;
+            Console.WriteLine("Dime 2 digitos");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            resultado = num1 + num2;
+            Console.WriteLine("Tu resultado es {0}", resultado);
         }
     }
 }
