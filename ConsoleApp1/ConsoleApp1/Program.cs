e0be2fd25b3aee4b7e5d8a2e09d6958227768373
Edson Sotomayor <esotomayor0@ucol.mx>
2019-08-14 21:29:36 -0500

    V3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,11 @@ namespace ConsoleApp1
             num2 = int.Parse(Console.ReadLine());
             resultado = num1 + num2;
             Console.WriteLine("Tu resultado es {0}", resultado);
+
+            string nombre;
+            Console.WriteLine("Dime tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola{0} ", nombre);
         }
     }
 }
